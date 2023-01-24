14c39b7e8860cb6aab2d8025d2ee2a638387e3f6
ncarfagno <ncarfagno@Ubuntu.myguest.virtualbox.org>
2023-01-24 18:48:48 -0500

    good now I think

---
+++ src/main/java/com/ncarfagno/restreactdemo/RestreactdemoApplication.java
@@ -0,0 +1,13 @@
+package com.ncarfagno.restreactdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestreactdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestreactdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ncarfagno/restreactdemo/RestreactdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncarfagno.restreactdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestreactdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
